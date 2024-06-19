a7fa8e216b14b93670369d1867a5c37f42407e90
諏訪子 <suwako@076.moe>
2024-06-20 01:50:04 +0900

    xrandrがなければ・・・

--- src/resolution.c
+++ src/resolution.c
@@ -1,8 +1,10 @@
 #include "resolution.h"
 #include "common.h"
 
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 const char *display_resolution() {
   const char *isexist = run_command_s("which xrandr");
@@ -10,7 +12,14 @@ const char *display_resolution() {
       isexist == NULL ||
       strlen(isexist) == 0 ||
       strncmp(isexist, "xrandr not found", strlen("xrandr not found")) == 0
-  ) return NULL;
+  ) {
+    if (isexist) free((void *)isexist);
+    return NULL;
+  }
+  free((void *)isexist);
+
+  const char *display = run_command_s("echo $DISPLAY");
+  if (display == NULL || strlen(display) == 0) return NULL;
 
   return run_command_s("xrandr --nograb --current | "
                        "awk -F 'connected |\\\\+|\\\\(' '/ "
