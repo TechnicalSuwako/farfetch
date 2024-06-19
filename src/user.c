f7d10e058c16452de087a3774fe6645fc995b61f
諏訪子 <suwako@076.moe>
2024-06-20 00:43:47 +0900

    全てのOSで「hostname」ってコマンドがあるから、OS別のコマンドが不要

--- src/user.c
+++ src/user.c
@@ -6,13 +6,5 @@ const char *display_user_name() {
 }
 
 const char *display_user_host() {
-#if defined(__OpenBSD__)
-  return run_command_s("cat /etc/myname");
-#elif defined(__NetBSD__)
   return run_command_s("hostname");
-#elif defined(__FreeBSD__)
-  return run_command_s("sysctl -n kern.hostname");
-#else
-  return run_command_s("cat /etc/hostname");
-#endif
 }
