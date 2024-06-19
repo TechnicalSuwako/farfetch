f01c5fd8a969e033d31bf606835fe0d32dd5e447
諏訪子 <suwako@076.moe>
2024-06-20 00:45:27 +0900

    xrandrがなければ（サーバやGaylord等）、解像度をスキップする

--- src/resolution.c
+++ src/resolution.c
@@ -1,7 +1,17 @@
 #include "resolution.h"
 #include "common.h"
 
+#include <stdio.h>
+#include <string.h>
+
 const char *display_resolution() {
+  const char *isexist = run_command_s("which xrandr");
+  if (
+      isexist == NULL ||
+      strlen(isexist) == 0 ||
+      strncmp(isexist, "xrandr not found", strlen("xrandr not found"))
+  ) return NULL;
+
   return run_command_s("xrandr --nograb --current | "
                        "awk -F 'connected |\\\\+|\\\\(' '/ "
                        "connected.*[0-9]+x[0-9]+\\+/ && $2 {printf $2 "
