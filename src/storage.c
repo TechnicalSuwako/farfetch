2fe1aa6de98d0f0c6c9e4a8dcf9192c6b3392883
諏訪子 <suwako@076.moe>
2024-06-21 22:26:50 +0900

    FreeBSDの場合の修正

--- src/storage.c
+++ src/storage.c
@@ -5,10 +5,15 @@
 
 const char *display_storage() {
   const char *iszfs = run_command_s("LC_ALL=C zpool list 2>&1");
-  if (strstr(iszfs, "command not found: zpool")) {
-    return run_command_s("zpool list | awk 'NR>1 {print $1 \": \" $3 \" / \" $2}' | "
-        "sed ':a;N;$!ba;s//, /g'");
+  if (!strstr(iszfs, "command not found: zpool")) {
+    return run_command_s("zpool list | "
+        "awk 'NR>1 {printf \"%s: %s / %s, \", $1, $3, $2}' | "
+        "awk '{sub(/, $/, \"\"); print}'");
   }
+
+  return run_command_s("df -h | "
+      "awk '/^\\/dev\\// {printf \"%s: %s / %s, \", $1, $3, $2}' | "
+      "awk '{sub(/, $/, \"\"); print}'");
 
   return run_command_s("df -h | awk 'NR>1 {print $1 \": \" $3 \" / \" $2}' | "
       "sed ':a;N;$!ba;s/\\n/, /g'");
