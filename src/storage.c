30c726806300796e31cf82548a064874f4f7705a
haturatu <taro@eyes4you.org>
2024-06-25 20:39:38 +0900

    fix indent

--- src/storage.c
+++ src/storage.c
@@ -5,17 +5,16 @@
 #include <stdlib.h>
 
 const char *display_storage() {
-	const char *excode = run_command_s("zpool list 2>/dev/null || echo $?");
-	if (
-			excode != NULL && (strncmp(excode, "127", strlen("127")) == 0)) {
-		free((void *)excode);
-		return run_command_s("df -h | "
-				"awk '/^\\/dev\\// {printf \"%s: %s / %s, \", $1, $3, $2}' | "
-				"awk '{sub(/, $/, \"\"); print}'");
-	} 
+  const char *excode = run_command_s("zpool list 2>/dev/null || echo $?");
+  if ( excode != NULL && (strncmp(excode, "127", strlen("127")) == 0)) {
+    free((void *)excode);
+    return run_command_s("df -h | "
+        "awk '/^\\/dev\\// {printf \"%s: %s / %s, \", $1, $3, $2}' | "
+        "awk '{sub(/, $/, \"\"); print}'");
+  } 
 
-	free((void *)excode);
-	return run_command_s("zpool list | "
-			"awk 'NR>1 {printf \"%s: %s / %s, \", $1, $3, $2}' | "
-			"awk '{sub(/, $/, \"\"); print}'");
+  free((void *)excode);
+  return run_command_s("zpool list | "
+      "awk 'NR>1 {printf \"%s: %s / %s, \", $1, $3, $2}' | "
+      "awk '{sub(/, $/, \"\"); print}'");
 }
